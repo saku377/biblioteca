99e9866aa039a4eae16353583c4d11b7ace858ad
saku377 <roxanahellen2001@gmail.com>
2026-03-23 12:35:47 -0300

    mensaje

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -15,5 +15,73 @@ public class LibroRepository {
     public List<Libro> obtenerLibros() {
         return listaLibros;
     }
+    //buscar un libro por su id
+    public Libro buscarPorId(int id){
+        for (Libro libro : listaLibros){
+            if (libro.getId() == id) {
+                return libro;
+                
+            }
+        }
+        return null;
+    }
 
+    //buscar un libro por su isbn
+    public Libro buscarPorIsbn(String isbn){
+        for (Libro libro : listaLibros){
+            if(libro.getIsbn().equals(isbn)){
+                return libro;
+            }
+        }
+        return null;
+    }
+    public Libro guardar(Libro lib){
+        listaLibros.add(lib);
+        return lib;
+    }
+    public Libro actualizar(Libro lib) {
+        int id = 0;
+        int idPosicion = 0;
+
+        for (int i =0; i < listaLibros.size(); i++){
+            if (listaLibros.get(i).getId() == lib.getId()){
+                id = lib.getId();
+                idPosicion = 1;
+            }
+        }
+
+        Libro libro1 = new Libro();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        listaLibros.set(idPosicion, libro1);
+        return libro1;
+    }
+
+    public void eliminar(int id) {
+        //alternativa 1
+        Libro libro = buscarPorId(id);
+        if (libro != null) {
+            listaLibros.remove(libro);
+        }
+       //alternativa 2
+        int idPosicion = 0;
+        for ( int i = 0; i< listaLibros.size(); i++) {
+            if (listaLibros.get(i). getId() == id) {
+                idPosicion = i;
+                break;
+            }
+
+        }
+        if (idPosicion > 0){
+            listaLibros.remove(idPosicion);
+        }
+
+        //otra alternativa
+        listaLibros.removeIf(X -> X.getId() == id);
+    }
 }
